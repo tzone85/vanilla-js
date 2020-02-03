ef5175fb855994feae46424d51350c85d6ec4ed0
Thando Mini <thando.mini@kurtosys.com>
2020-02-03 22:40:09 +0200

    added other validation function checks, for lengths, max and min as well as confirming that the passwords match.

--- script.js
+++ script.js
@@ -20,9 +20,14 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidemail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    //return re.test(String(input).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `Email is not valid`);
+    }
 }
 
 // Check required fields
@@ -36,6 +41,24 @@ function checkRequired(inputArr) {
     })
 }
 
+// check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min ) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must not be less than ${max} characters`)
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
 // Get fieldname
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -46,4 +69,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 20);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
